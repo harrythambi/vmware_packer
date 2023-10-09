e74946aefae7fe9d0a56642a8c7992db44ce09d6
Harry Thambi <harry.thambi@outlook.com>
2023-10-09 20:35:38 +0000

    update files

--- windows10/setup/cloudbase-init-windows.ps1
+++ windows10/setup/cloudbase-init-windows.ps1
@@ -1,38 +1,52 @@
-# Installs 64-bit Cloudbase-init on Microsoft Windows system
+$ErrorActionPreference = "Stop"
 
-$cloudbaseInitVersion = "1.1.2"
+# Disable system hibernation
+Write-Host "-- Disabling system hibernation ..."
+Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Power\" -Name "HiberFileSizePercent" -Value 0 | Out-Null
+Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Power\" -Name "HibernateEnabled" -Value 0 | Out-Null
 
-### DO NOT EDIT BEYOND HERE ###
+# Disable password expiration for Administrator
+Write-Host "-- Disabling password expiration for local Administrator user ..."
+Set-LocalUser Administrator -PasswordNeverExpires $true
 
-$cloudbaseInitVersionUnderscore = $cloudbaseInitVersion.replace(".","_")
-$cloudbaseInitInstallerUri = "https://github.com/cloudbase/cloudbase-init/releases/download/${cloudbaseInitVersion}/CloudbaseInitSetup_${cloudbaseInitVersionUnderscore}_x64.msi"
-$cloudbaseInitInstaller = Split-Path $cloudbaseInitInstallerUri -Leaf
+# Disable TLS 1.0
+Write-Host "-- Disabling TLS 1.0 ..."
+New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols" -Name "TLS 1.0" | Out-Null
+New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0" -Name "Server" | Out-Null
+New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0" -Name "Client" | Out-Null
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client" -Name "Enabled" -Value 0 | Out-Null
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client" -Name "DisabledByDefault" -Value 1 | Out-Null
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server" -Name "Enabled" -Value 0 | Out-Null
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server" -Name "DisabledByDefault" -Value 1 | Out-Null
+ 
+# Disable TLS 1.1
+Write-Host "-- Disabling TLS 1.1 ..."
+New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols" -Name "TLS 1.1" | Out-Null
+New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1" -Name "Server" | Out-Null
+New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1" -Name "Client" | Out-Null
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client" -Name "Enabled" -Value 0 | Out-Null
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client" -Name "DisabledByDefault" -Value 1 | Out-Null
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server" -Name "Enabled" -Value 0 | Out-Null
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server" -Name "DisabledByDefault" -Value 1 | Out-Null
 
-$cloudbaseInitInstallPath = "C:\Program Files\Cloudbase Solutions\Cloudbase-Init\conf\"
-$cloudbaseInitConfigFile = "cloudbase-init.conf"
-$cloudbaseInitUnattendConfigFile = "cloudbase-init-unattend.conf"
+# Install OpenSSH
+Write-Host "-- Installing OpenSSH ..."
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0 | Out-Null
+Set-Service sshd -StartupType Automatic | Out-Null
+New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force | Out-Null
 
-Write-Host "Downloading Cloudbase-Init Installer ..."
-Invoke-WebRequest -Uri $cloudbaseInitInstallerUri -OutFile C:\$cloudbaseInitInstaller
-
-Write-Host "Unlocking Cloudbase-Init Installer ..."
-Unblock-File -Path C:\$cloudbaseInitInstaller
-
-Write-Host "Installing Cloudbase-Init ..."
-Start-Process msiexec.exe -ArgumentList "/i C:\$cloudbaseInitInstaller /qn /norestart RUN_SERVICE_AS_LOCAL_SYSTEM=1" -Wait
-
-Write-Host "Removing the default Cloudbase-Init configuration files ..."
-Remove-Item -Path ($cloudbaseInitInstallPath + $cloudbaseInitConfigFile) -Confirm:$false
-Remove-Item -Path ($cloudbaseInitInstallPath + $cloudbaseInitUnattendConfigFile) -Confirm:$false
-
+# Installing Cloudbase-Init
+Write-Host "-- Installing Cloudbase-Init ..."
+$msiLocation = 'https://cloudbase.it/downloads'
+$msiFileName = 'CloudbaseInitSetup_Stable_x64.msi'
+Invoke-WebRequest -Uri ($msiLocation + '/' + $msiFileName) -OutFile C:\$msiFileName
+Unblock-File -Path C:\$msiFileName
+Start-Process msiexec.exe -ArgumentList "/i C:\$msiFileName /qn /norestart RUN_SERVICE_AS_LOCAL_SYSTEM=1" -Wait
+Write-Host " - Configuring Cloudbase-Init for VMwareGuestInfoService ..."
+$confFile = 'cloudbase-init.conf'
+$confPath = "C:\Program Files\Cloudbase Solutions\Cloudbase-Init\conf\"
 $confContent = @"
 [DEFAULT]
-username=Admin
-groups=Administrators
-inject_user_password=true
-config_drive_raw_hhd=true
-config_drive_cdrom=true
-config_drive_vfat=true
 bsdtar_path=C:\Program Files\Cloudbase Solutions\Cloudbase-Init\bin\bsdtar.exe
 mtools_path=C:\Program Files\Cloudbase Solutions\Cloudbase-Init\bin\
 verbose=true
@@ -40,48 +54,19 @@ debug=true
 logdir=C:\Program Files\Cloudbase Solutions\Cloudbase-Init\log\
 logfile=cloudbase-init.log
 default_log_levels=comtypes=INFO,suds=INFO,iso8601=WARN,requests=WARN
-logging_serial_port_settings=
-mtu_use_dhcp_config=true
-ntp_use_dhcp_config=true
 local_scripts_path=C:\Program Files\Cloudbase Solutions\Cloudbase-Init\LocalScripts\
-check_latest_version=true
 metadata_services=cloudbaseinit.metadata.services.vmwareguestinfoservice.VMwareGuestInfoService
-plugins=cloudbaseinit.plugins.common.ephemeraldisk.EphemeralDiskPlugin,cloudbaseinit.plugins.common.mtu.MTUPlugin,cloudbaseinit.plugins.common.sethostname.SetHostNamePlugin,cloudbaseinit.plugins.common.sshpublickeys.SetUserSSHPublicKeysPlugin,cloudbaseinit.plugins.common.userdata.UserDataPlugin,cloudbaseinit.plugins.common.localscripts.LocalScriptsPlugin,cloudbaseinit.plugins.windows.createuser.CreateUserPlugin
+plugins=cloudbaseinit.plugins.common.userdata.UserDataPlugin
 "@
-
-Write-Host "Creating new Cloudbase-Init ${cloudbaseInitConfigFile} File ..."
-New-Item -Path $cloudbaseInitInstallPath -Name $cloudbaseInitConfigFile -ItemType File -Force -Value $confContent | Out-Null
-
-$unattendContent = @"
-[DEFAULT]
-username=Admin
-groups=Administrators
-inject_user_password=true
-config_drive_raw_hhd=true
-config_drive_cdrom=true
-config_drive_vfat=true
-bsdtar_path=C:\Program Files\Cloudbase Solutions\Cloudbase-Init\bin\bsdtar.exe
-mtools_path=C:\Program Files\Cloudbase Solutions\Cloudbase-Init\bin\
-verbose=true
-debug=true
-logdir=C:\Program Files\Cloudbase Solutions\Cloudbase-Init\log\
-logfile=cloudbase-init-unattend.log
-default_log_levels=comtypes=INFO,suds=INFO,iso8601=WARN,requests=WARN
-logging_serial_port_settings=
-mtu_use_dhcp_config=true
-ntp_use_dhcp_config=true
-local_scripts_path=C:\Program Files\Cloudbase Solutions\Cloudbase-Init\LocalScripts\
-check_latest_version=false
-metadata_services=cloudbaseinit.metadata.services.vmwareguestinfoservice.VMwareGuestInfoService
-plugins=cloudbaseinit.plugins.common.mtu.MTUPlugin
-allow_reboot=false
-stop_service_on_exit=false
-"@
-Write-Host "Creating new Cloudbase-Init ${cloudbaseInitUnattendConfigFile} File ..."
-New-Item -Path $cloudbaseInitInstallPath -Name $cloudbaseInitUnattendConfigFile -ItemType File -Force -Value $unattendContent | Out-Null
-
-Write-Host "Enabling automatic startup for Cloudbase-init ..."
+New-Item -Path $confPath -Name $confFile -ItemType File -Force -Value $confContent | Out-Null
 Start-Process sc.exe -ArgumentList "config cloudbase-init start= delayed-auto" -wait | Out-Null
+Remove-Item -Path ($confPath + "cloudbase-init-unattend.conf") -Confirm:$false 
+Remove-Item -Path ($confPath + "Unattend.xml") -Confirm:$false 
+Remove-Item C:\$msiFileName -Confirm:$false
 
-Write-Host "Cleaning up Cloudbase-Init installer ..."
-Remove-Item C:\$cloudbaseInitInstaller -Confirm:$false
+# Enabling RDP connections
+Write-Host "-- Enabling RDP connections ..."
+Start-Process netsh -ArgumentList 'advfirewall firewall set rule group="Remote Desktop" new enable=yes' -wait | Out-Null
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0 | Out-Null
+
+Write-Host "-- Configuration complete ..."
--- windows10/setup/setup.ps1
+++ windows10/setup/setup.ps1
@@ -1,26 +1,25 @@
 $ErrorActionPreference = "Stop"
- 
+
 # Switch network connection to private mode
 # Required for WinRM firewall rules
 $profile = Get-NetConnectionProfile
+While ($profile.Name -eq "Identifying..."){
+    Start-Sleep -Seconds 10
+    $profile = Get-NetConnectionProfile
+}
 Set-NetConnectionProfile -Name $profile.Name -NetworkCategory Private
- 
+
+# Drop the firewall while building and re-enable as a standalone provisioner in the Packer file if needs be
+netsh Advfirewall set allprofiles state off
+
 # Enable WinRM service
 winrm quickconfig -quiet
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
- 
+
+# Disable IPV6
+Disable-NetAdapterBinding -Name "*" -ComponentID ms_tcpip6
+
 # Reset auto logon count
 # https://docs.microsoft.com/en-us/windows-hardware/customize/desktop/unattend/microsoft-windows-shell-setup-autologon-logoncount#logoncount-known-issue
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name AutoLogonCount -Value 0
-
-
-# dism /online /quiet /set-edition:{{win_edition}} /productkey:WX4NM-KYWYW-QJJR4-XV3QB-6VM33 /accepteula
-
-# https://packages.vmware.com/tools/esx/7.0u3/windows/x64/VMware-tools-11.3.0-18090558-x86_64.exe
-# https://packages.vmware.com/tools/releases/11.3.0/windows/VMware-tools-windows-11.3.0-18090558.iso
-# $packagesVMWareURLList = "http://packages.vmware.com/tools/esx/7.0u3/windows/x64"
-# $latestVMwareTools = ((curl $packagesVMWareURL).links[1].href).split("/")[1]
-# $latestVMwareToolsURL = $packagesVMWareURL+"/"+$latestVMwareTools
-# Invoke-WebRequest -Uri $latestVMwareToolsURL -OutFile "C:\Windows\temp\vmware-tools.exe"
-# C:\Windows\temp\C:\Windows\temp\vmware-tools.exee /S /v"/qn REBOOT=R"
