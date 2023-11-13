6632bce19b7fc6f967f5e3f447ad9ee93eca16cf
Harry Thambi <harry.thambi@outlook.com>
2023-11-13 16:06:14 +0000

    update win2022 scripts

--- win2022/setup/setup.ps1
+++ win2022/setup/setup.ps1
@@ -17,25 +17,25 @@ Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Power\" -Name "Hibernat
 Write-Host "-- Disabling password expiration for local Administrator user ..."
 Set-LocalUser Administrator -PasswordNeverExpires $true
 
-# Disable TLS 1.0
-Write-Host "-- Disabling TLS 1.0 ..."
-New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols" -Name "TLS 1.0" | Out-Null
-New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0" -Name "Server" | Out-Null
-New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0" -Name "Client" | Out-Null
-New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client" -Name "Enabled" -Value 0 | Out-Null
-New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client" -Name "DisabledByDefault" -Value 1 | Out-Null
-New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server" -Name "Enabled" -Value 0 | Out-Null
-New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server" -Name "DisabledByDefault" -Value 1 | Out-Null
+# # Disable TLS 1.0
+# Write-Host "-- Disabling TLS 1.0 ..."
+# New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols" -Name "TLS 1.0" | Out-Null
+# New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0" -Name "Server" | Out-Null
+# New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0" -Name "Client" | Out-Null
+# New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client" -Name "Enabled" -Value 0 | Out-Null
+# New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client" -Name "DisabledByDefault" -Value 1 | Out-Null
+# New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server" -Name "Enabled" -Value 0 | Out-Null
+# New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server" -Name "DisabledByDefault" -Value 1 | Out-Null
  
-# Disable TLS 1.1
-Write-Host "-- Disabling TLS 1.1 ..."
-New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols" -Name "TLS 1.1" | Out-Null
-New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1" -Name "Server" | Out-Null
-New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1" -Name "Client" | Out-Null
-New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client" -Name "Enabled" -Value 0 | Out-Null
-New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client" -Name "DisabledByDefault" -Value 1 | Out-Null
-New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server" -Name "Enabled" -Value 0 | Out-Null
-New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server" -Name "DisabledByDefault" -Value 1 | Out-Null
+# # Disable TLS 1.1
+# Write-Host "-- Disabling TLS 1.1 ..."
+# New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols" -Name "TLS 1.1" | Out-Null
+# New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1" -Name "Server" | Out-Null
+# New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1" -Name "Client" | Out-Null
+# New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client" -Name "Enabled" -Value 0 | Out-Null
+# New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client" -Name "DisabledByDefault" -Value 1 | Out-Null
+# New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server" -Name "Enabled" -Value 0 | Out-Null
+# New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server" -Name "DisabledByDefault" -Value 1 | Out-Null
 
 # Set network connections provile to Private mode.
 Write-Output 'Setting the network connection profiles to Private...'
@@ -67,34 +67,34 @@ Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0 | Out-Null
 Set-Service sshd -StartupType Automatic | Out-Null
 New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force | Out-Null
 
-# # Installing Cloudbase-Init
-# Write-Host "-- Installing Cloudbase-Init ..."
-# $msiLocation = 'https://cloudbase.it/downloads'
-# $msiFileName = 'CloudbaseInitSetup_Stable_x64.msi'
-# Invoke-WebRequest -Uri ($msiLocation + '/' + $msiFileName) -OutFile C:\$msiFileName
-# Unblock-File -Path C:\$msiFileName
-# Start-Process msiexec.exe -ArgumentList "/i C:\$msiFileName /qn /norestart RUN_SERVICE_AS_LOCAL_SYSTEM=1" -Wait
-# Write-Host " - Configuring Cloudbase-Init for VMwareGuestInfoService ..."
-# $confFile = 'cloudbase-init.conf'
-# $confPath = "C:\Program Files\Cloudbase Solutions\Cloudbase-Init\conf\"
-# $confContent = @"
-# [DEFAULT]
-# bsdtar_path=C:\Program Files\Cloudbase Solutions\Cloudbase-Init\bin\bsdtar.exe
-# mtools_path=C:\Program Files\Cloudbase Solutions\Cloudbase-Init\bin\
-# verbose=true
-# debug=true
-# logdir=C:\Program Files\Cloudbase Solutions\Cloudbase-Init\log\
-# logfile=cloudbase-init.log
-# default_log_levels=comtypes=INFO,suds=INFO,iso8601=WARN,requests=WARN
-# local_scripts_path=C:\Program Files\Cloudbase Solutions\Cloudbase-Init\LocalScripts\
-# metadata_services=cloudbaseinit.metadata.services.vmwareguestinfoservice.VMwareGuestInfoService
-# plugins=cloudbaseinit.plugins.common.userdata.UserDataPlugin
-# "@
-# New-Item -Path $confPath -Name $confFile -ItemType File -Force -Value $confContent | Out-Null
-# Start-Process sc.exe -ArgumentList "config cloudbase-init start= delayed-auto" -wait | Out-Null
-# Remove-Item -Path ($confPath + "cloudbase-init-unattend.conf") -Confirm:$false 
-# Remove-Item -Path ($confPath + "Unattend.xml") -Confirm:$false 
-# Remove-Item C:\$msiFileName -Confirm:$false
+# Installing Cloudbase-Init
+Write-Host "-- Installing Cloudbase-Init ..."
+$msiLocation = 'https://cloudbase.it/downloads'
+$msiFileName = 'CloudbaseInitSetup_Stable_x64.msi'
+Invoke-WebRequest -Uri ($msiLocation + '/' + $msiFileName) -OutFile C:\$msiFileName
+Unblock-File -Path C:\$msiFileName
+Start-Process msiexec.exe -ArgumentList "/i C:\$msiFileName /qn /norestart RUN_SERVICE_AS_LOCAL_SYSTEM=1" -Wait
+Write-Host " - Configuring Cloudbase-Init for VMwareGuestInfoService ..."
+$confFile = 'cloudbase-init.conf'
+$confPath = "C:\Program Files\Cloudbase Solutions\Cloudbase-Init\conf\"
+$confContent = @"
+[DEFAULT]
+bsdtar_path=C:\Program Files\Cloudbase Solutions\Cloudbase-Init\bin\bsdtar.exe
+mtools_path=C:\Program Files\Cloudbase Solutions\Cloudbase-Init\bin\
+verbose=true
+debug=true
+logdir=C:\Program Files\Cloudbase Solutions\Cloudbase-Init\log\
+logfile=cloudbase-init.log
+default_log_levels=comtypes=INFO,suds=INFO,iso8601=WARN,requests=WARN
+local_scripts_path=C:\Program Files\Cloudbase Solutions\Cloudbase-Init\LocalScripts\
+metadata_services=cloudbaseinit.metadata.services.vmwareguestinfoservice.VMwareGuestInfoService
+plugins=cloudbaseinit.plugins.common.userdata.UserDataPlugin
+"@
+New-Item -Path $confPath -Name $confFile -ItemType File -Force -Value $confContent | Out-Null
+Start-Process sc.exe -ArgumentList "config cloudbase-init start= delayed-auto" -wait | Out-Null
+Remove-Item -Path ($confPath + "cloudbase-init-unattend.conf") -Confirm:$false 
+Remove-Item -Path ($confPath + "Unattend.xml") -Confirm:$false 
+Remove-Item C:\$msiFileName -Confirm:$false
 
 # Enabling RDP connections
 Write-Host "-- Enabling RDP connections ..."
